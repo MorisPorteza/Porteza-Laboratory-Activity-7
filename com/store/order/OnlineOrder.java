6e1864e904dc26ef3e8d94281f9acbc892fbccfb
Moris L. Porteza <morisporteza60@gmail.com>
2026-01-23 15:24:40 +0800

    Update and rename OnlineOrder.java to com/store/order/OnlineOrder.java
    Added OnlineOrder class that extends Order and implements Payable interface with processOrder and pay methods.

--- com/store/order/OnlineOrder.java
+++ com/store/order/OnlineOrder.java
@@ -2,17 +2,23 @@ package com.store.order;
 
 import com.store.payment.Payable;
 
+// OnlineOrder is a specific type of Order.
+// It extends Order to reuse common behavior and implements Payable to guarantee payment capability.
 public class OnlineOrder extends Order implements Payable {
 
     public OnlineOrder(int orderId, double amount) {
         super(orderId, amount);
     }
 
+    // Concrete implementation of abstract method.
+    // This is required because Order declares processOrder() as abstract.
     @Override
     public void processOrder() {
         System.out.println("Processing online order ID: " + getOrderId());
     }
 
+    // Implements Payable interface.
+    // This updates status safely using protected setter.
     @Override
     public void pay() {
         setStatus(OrderStatus.PAID);
